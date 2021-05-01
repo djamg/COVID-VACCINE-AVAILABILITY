d4e7e3213642d9d5b4bf9f5321f0474d24286967
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2021-05-01 22:30:05 +0530

    Filtered for 2 days

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import requests
 import datetime
 import json
+from datetime import timedelta
 
 date_object = datetime.date.today()
 date_str = date_object.strftime("%d-%m-%Y")
-print(date_str)
+# print(date_str)
+
+one_day = date_object + timedelta(days=1)
+# print(one_day.strftime("%d-%m-%Y"))
+
+two_day = date_object + timedelta(days=2)
+# print(two_day.strftime("%d-%m-%Y"))
 
 district = 294
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(
@@ -19,7 +26,6 @@ for i in response_json["centers"]:
     for j in i["sessions"]:
         # print(j["available_capacity"])
         if j["available_capacity"] > 0:
-            print(str(j["available_capacity"]) + " vaccines are available at: " +
-                  i['name'] + " on " + j['date'] + " between " + str(j['slots']))
-            # print("Vaccine is available at: " +
-            #       i['name'] + ", on: " + j['date'] + " at: " + j['slots'])
+            if j["date"] == one_day.strftime("%d-%m-%Y"):
+                print(str(j["available_capacity"]) + " " + j["vaccine"] + " vaccines are available at: " +
+                      i['name'] + " on " + j['date'] + " between " + str(j['slots']))
