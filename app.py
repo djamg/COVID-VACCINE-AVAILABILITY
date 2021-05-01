11ed96ceed3cf7f2cf900bd196b50f8182ea8ff3
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2021-05-01 22:42:14 +0530

    Added ages 18

--- app.py
+++ app.py
@@ -4,28 +4,37 @@ import json
 from datetime import timedelta
 
 date_object = datetime.date.today()
-date_str = date_object.strftime("%d-%m-%Y")
-# print(date_str)
+current_day_str = date_object.strftime("%d-%m-%Y")
+# print(current_day_str)
 
 one_day = date_object + timedelta(days=1)
+one_day_str = one_day.strftime("%d-%m-%Y")
 # print(one_day.strftime("%d-%m-%Y"))
 
 two_day = date_object + timedelta(days=2)
+two_day_str = two_day.strftime("%d-%m-%Y")
 # print(two_day.strftime("%d-%m-%Y"))
 
 district = 294
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(
-    str(district), date_str)
+    str(district), current_day_str)
 
 response = requests.request("GET", url)
 response_json = response.json()
 # print(response_json["centers"])
 
-for i in response_json["centers"]:
-    # print(i["sessions"])
-    for j in i["sessions"]:
-        # print(j["available_capacity"])
-        if j["available_capacity"] > 0:
-            if j["date"] == one_day.strftime("%d-%m-%Y"):
-                print(str(j["available_capacity"]) + " " + j["vaccine"] + " vaccines are available at: " +
-                      i['name'] + " on " + j['date'] + " between " + str(j['slots']))
+
+def search(day="current_day_str", age=45, availablilty=1):
+    print("For age {} and above: ".format(age))
+    for i in response_json["centers"]:
+        # print(i["sessions"])
+        for j in i["sessions"]:
+            # print(j["available_capacity"])
+            if j["available_capacity"] >= availablilty:
+                if j["min_age_limit"] == age:
+                    if j["date"] == one_day.strftime("%d-%m-%Y"):
+                        print(str(j["available_capacity"]) + " " + j["vaccine"] + " vaccines are available at: " +
+                              i['name'] + " on " + j['date'] + " between " + str(j['slots']))
+
+
+search(day="current_day_str", age=18, availablilty=1)
